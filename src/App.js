1aca423692f1f40d6455b1951c658bfb6ab2f9d0
Sarah Maples <sarah.e.maples@gmail.com>
2019-05-13 14:10:39 -0500

    chatscreen not working but i need to research bonnie and clyde

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react'
+import UsernameForm from './components/UsernameForm'
+import ChatScreen from './ChatScreen'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentScreen: "WhatIsYourUsernameScreen",
+      currentUsername: ""
+    }
+    this.onUsernameSubmitted.bind(this);
+  }
+
+  onUsernameSubmitted(username) {
+    fetch("http://localhost:3001/users", {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ username }),
+    })
+      .then(res => {
+        console.log('success');
+        console.log(username);
+        this.setState({
+          currentUsername: username,
+          currentScreen: 'ChatScreen',
+        })
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   render() {
-    return <h1>Slack</h1>
+    if (this.state.currentScreen === "WhatIsYourUsernameScreen") {
+      return <UsernameForm onSubmit={this.onUsernameSubmitted}></UsernameForm>
+    } else if (this.state.currentScreen === 'ChatScreen') {
+      return <ChatScreen currentUsername={this.state.currentUsername} />
+    }
   }
 }
 
