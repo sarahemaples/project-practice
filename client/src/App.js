41e23fa5ef52ccd4c83461a594c0eba7e905de5a
Sarah Maples <sarah.e.maples@gmail.com>
2019-05-15 19:04:07 -0500

    no more api errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import Chatkit from '@pusher/chatkit-client';
 import MessageList from './components/MessageList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <MessageList />
-    </div>
-  );
+class App extends React.Component{
+
+  componentDidMount(){
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: 'v1:us1:758e334a-5a1d-4660-8590-24de4fb4637f',
+      userId: "sarah",
+      tokenProvider: new Chatkit.TokenProvider({
+        url: "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/758e334a-5a1d-4660-8590-24de4fb4637f/token"
+      })
+    });
+
+    chatManager.connect()
+    .then(currentUser => {
+      currentUser.subscribeToRoom({
+        roomId: '19422811',
+        hooks: {
+          onNewMessage: message => {
+            console.log('message.text: ', message.text);
+          }
+        }
+      })
+    })
+
+  }
+  render() {
+    return (
+      <div className="App">
+          <MessageList />
+      </div>
+    )
+  }
+
 }
 
 export default App;
--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -21,6 +21,7 @@ class MessageList extends React.Component {
             <div className="message-list">
                 {DUMMY_DATA.map((message, index) => {
                     return (
+                        // not recommended to use index but for now its good
                         <div key={index} className="message">
                             <div className="message-username">{message.senderId}</div>
                             <div className="message-text">{message.text}</div>
